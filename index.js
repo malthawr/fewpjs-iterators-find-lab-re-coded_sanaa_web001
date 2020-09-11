e82e7c7f420f97c0ad6072bb4ad3a595eb471830
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-11 05:16:53 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let superbowlWin = (record) => {
+    let result = record.find(record => record.result === "W")
+
+    if (!!result) {
+        return result.year;
+    } else {
+
+        return  undefined;
+    }
+}
